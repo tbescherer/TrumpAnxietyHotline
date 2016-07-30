c4b6f6d2f60923fc0badc0dbf96d736fd79523ca
Tom Bescherer <tbescherer@gmail.com>
2016-07-30 13:51:49 -0400

    let chatters know if other participant has connected

--- components/Messenger/Messenger.js
+++ components/Messenger/Messenger.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import store from '../../core/store.js';
+import {keycodes} from './constants.js';
 
 class Messenger extends React.Component {
     constructor(props) {
@@ -7,7 +8,10 @@ class Messenger extends React.Component {
         this.post = this.post.bind(this);
         this.changeBlogText = this.changeBlogText.bind(this);
         this.renderPosts = this.renderPosts.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.postAdminMessage = this.postAdminMessage.bind(this);
         this.state = {
+            alone: true,
             user_id: "",
             posts: [],
             blogText: "",
@@ -26,18 +30,35 @@ class Messenger extends React.Component {
                 update['open_conversation'] = newConvoKey;
                 update[newConvoKey] = {};
                 return update;
-            } else if (conversation.open_conversation && conversation[conversation.open_conversation]["participant1"] !== firebase.auth().currentUser.uid) {
+            } else if (conversation.open_conversation && conversation[conversation.open_conversation]["participant1"]["id"] !== firebase.auth().currentUser.uid) {
                 console.log("no second participant");
                 newConvoKey = conversation.open_conversation;
-                conversation[newConvoKey]["participant2"] = firebase.auth().currentUser.uid;
+                let now = new Date();
+                let participantData = {
+                    'id': firebase.auth().currentUser.uid,
+                    'connected_datetime': now.toDateString()
+                }
+                that.setState({'alone': false});
+                conversation[newConvoKey]["participant2"] = participantData;
                 conversation['open_conversation'] = false;
                 return conversation;
             } else {
                 conversation['open_conversation'] = newConvoKey;
-                conversation[newConvoKey
-                ] = {
-                    'participant1': firebase.auth().currentUser.uid
+                let now = new Date();
+                let participantData = {
+                    'id': firebase.auth().currentUser.uid,
+                    'connected_datetime': now.toDateString()
+                }
+                conversation[newConvoKey] = {
+                    'participant1': participantData
                 };
+                var participantRef = firebase.database().ref('conversations/' + newConvoKey + '/participant2').limitToLast(1);
+                participantRef.on('value', function(data){
+                    console.log(data.val());
+                    if (data.val() !== null) {
+                        that.setState({'alone': false})
+                    }
+                })
                 return conversation;
             }
         }, function(error, committed, snapshot){
@@ -48,9 +69,21 @@ class Messenger extends React.Component {
             });
             recentPostsRef.on('child_removed', function(data){
                 store.dispatch({type:'DELETE_POST', data: {'posts': data.val()}});
-            })
+            });
         });
     };
+
+    postAdminMessage(conversationsRef, conversationID, postBody) {
+        let now = new Date();
+        let newPostKey = firebase.database().ref().child('conversations').push().key;
+        let postData = {
+            'text': postBody,
+            'user_id': 'admin',
+            'post_datetime': now.toDateString(),
+        }
+        conversationsRef[conversationID]["posts"][newPostKey] = postData;
+        return conversationsRef;
+    }
 
     componentWillMount() {
         let that = this;
@@ -69,12 +102,13 @@ class Messenger extends React.Component {
         let newPostKey = firebase.database().ref().child('conversations').push().key;
         let update = {}
         let that = this;
+        let now = new Date();
         console.log(firebase.auth().currentUser.uid);
         let postData = {
             'text': this.state.blogText,
             'user_id': firebase.auth().currentUser.uid,
+            'post_datetime': now.toDateString(),
         }
-        console.log(this.state.conversationID)
         update['/conversations/' + this.state.conversationID + "/posts/" + newPostKey] = postData;
         firebase.database().ref().update(update).then(function() {
             that.setState({blogText: ""});
@@ -107,7 +141,8 @@ class Messenger extends React.Component {
         })
         return (
             <div style={{width: '90%', margin: 'auto', marginTop: '10px', paddingBottom: '10px'}} className="mdl-card mdl-shadow--2dp">
-                <div className="mdl-card__title" style={{fontSize: '24px'}}>Your Conversation</div>
+                <div className="mdl-card__title" style={{fontSize: '24px'}}>Your Shared Anxiety</div>
+                {this.state.alone ? <div>You got here first</div> : <div>You both connected</div>}
                 <div className="mdl-card__supporting-text">
                 {posts}
                 </div>
@@ -116,11 +151,20 @@ class Messenger extends React.Component {
         )
     }
 
+    handleKeyDown (e) {
+        var that = this;
+        if (e.keyCode == keycodes.ENTER) {
+            that.post();
+        } else {
+            console.log(e)
+        }
+    }
+
     renderPostArea() {
         return (
             <div style={{paddingLeft: '20px'}}>
                 <div className="mdl-textfield mdl-js-textfield">
-                    <input onChange={this.changeBlogText} value={this.state.blogText} className="mdl-textfield__input" type="text" id="sample1"/>
+                    <input onChange={this.changeBlogText} onKeyDown={this.handleKeyDown} value={this.state.blogText} className="mdl-textfield__input" type="text" id="sample1"/>
                     <label className="mdl-textfield__label" htmlFor="sample1">Text...</label>
                 </div>
                 <button onClick={this.post} className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored">
---
+++ components/Messenger/constants.js
@@ -0,0 +1,6 @@
+export const keycodes = {
+    ENTER: 13,
+    UP: 38,
+    DOWN: 40,
+    TAB: 9
+}
